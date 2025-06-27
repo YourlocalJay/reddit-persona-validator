b96423e57de31cec8f71bd3f5b9b6c8491681c95
YourlocalJay <125357195+YourlocalJay@users.noreply.github.com>
2025-06-26 20:08:43 -0700

    Update interfaces __init__.py with exports

--- src/interfaces/__init__.py
+++ src/interfaces/__init__.py
@@ -1,1 +1,10 @@
-# Interfaces module initialization
+"""Interface initialization module for Reddit Persona Validator."""
+
+from typing import Dict, Optional, Any
+
+# Import interfaces
+from .cli import PersonaValidatorCLI
+from .api import app as api_app
+from .gui import RedditPersonaValidatorGUI
+
+__all__ = ["PersonaValidatorCLI", "api_app", "RedditPersonaValidatorGUI"]
